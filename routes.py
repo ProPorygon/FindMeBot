bd4ecd9f8f1aad40dbe91c4f998fd2d6872883a1
Kevin Renner <krenner529@gmail.com>
2017-02-25 04:12:46 -0600

    h

--- routes.py
+++ routes.py
@@ -8,8 +8,9 @@ def defaut():
     data = json.loads(json_message)
     message = data["text"]
     user = data["user_id"]
-    url = data["attachments"][0].get("url")
-    attachment_type = data["attachments"][0].get("type")
+    if len(data["attachments"]) > 0:
+        url = data["attachments"][0]["url"]
+        attachment_type = data["attachments"][0]["type"]
     print url
     print message
     print user
