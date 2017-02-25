664d72c0597812d333936f3ace564e2bab550220
Kevin Renner <krenner529@gmail.com>
2017-02-25 03:10:22 -0600

    more stuff

--- routes.py
+++ routes.py
@@ -1,5 +1,35 @@
-from bottle import route
+"""
+Routes and views for the bottle application.
+"""
+
+from bottle import route, view
+from datetime import datetime
 
 @route('/')
-def defaut():
-    return "Hello World"
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
+
+@route('/contact')
+@view('contact')
+def contact():
+    """Renders the contact page."""
+    return dict(
+        title='Contact',
+        message='Your contact page.',
+        year=datetime.now().year
+    )
+
+@route('/about')
+@view('about')
+def about():
+    """Renders the about page."""
+    return dict(
+        title='About',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
