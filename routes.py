43e313e8ddc8c2ae280ec8758678d5c33e02eeb6
Kevin Renner <krenner529@gmail.com>
2017-02-25 11:40:45 -0600

    moving database stuff
    b
    
    c
    
    d
    
    e
    
    f

--- routes.py
+++ routes.py
@@ -8,6 +8,21 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+url = urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+con = psycopg2.connect(
+    dbname = dbname,
+    user = user,
+    password = password,
+    host = host,
+    port = port
+)
+
 @route('/', method='POST')
 def defaut():
     json_message = request.body.read()
@@ -33,28 +48,13 @@ Function to associate a user name with an image
 TODO: Determine how to store image
 '''
 def save_image(user, url):
-    url = urlparse(os.environ['DATABSE_URL'])
-    dbname = url.path[1:]
-    user = url.username
-    password = url.password
-    host = url.hostname
-    port = url.port
-
-    con = psycopg2.connect(
-        dbname = dbname,
-        user = user,
-        password = password,
-        host = host,
-        port = port
-    )
-
-    curs = con.cursor()
+    cur = con.cursor()
 
     response = requests.get(url)
     data = BytesIO(response.content)
 
-    binary = psycopg2.Binary(data)
-    cur.execute("INSERT INTO userimages(id, image) VALUES (%s, %s)", user, (binary,))
+    binary = psycopg2.Binary(response.content)
+    cur.execute("INSERT INTO userimage(id, image) VALUES (%s, %s)", (user,binary))
 
     print "success"
 
