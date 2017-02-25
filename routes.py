da6ac6b8e7836a5ef16443627ab6535bb71cab0c
Kevin Renner <krenner529@gmail.com>
2017-02-25 11:25:56 -0600

    testing postgres

--- routes.py
+++ routes.py
@@ -1,6 +1,12 @@
 from bottle import route, request
 import logging
 import json
+import psycopg2
+import os
+import urllib.parse as urlparse
+import requests
+from PIL import Image
+from io import BytesIO
 
 @route('/', method='POST')
 def defaut():
@@ -14,8 +20,8 @@ def defaut():
         url = data["attachments"][0]["url"]
         attachment_type = data["attachments"][0]["type"]
 
-    if message.split(" ")[,1] == "This is":
-        save_image(message.split(" ")[2], url)
+    if message.lower() == "this is me":
+        save_image(user, url)
 
     print url
     print message
@@ -27,4 +33,27 @@ Function to associate a user name with an image
 TODO: Determine how to store image
 '''
 def save_image(user, url):
+    url = urlparse.urlparse(os.environ['DATABSE_URL'])
+    dbname = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
+
+    con = psycopg2.connect(
+        dbname = dbname,
+        user = user,
+        password = password,
+        host = host,
+        port = port
+    )
+
+    curs = con.cursor()
+
+    response = requests.get(url)
+    data = BytesIO(response.content)
+
+    binary = psycopg2.Binary(data)
+    cur.execute("INSERT INTO userimages(id, image) VALUES (%s, %s)", user, (binary,))
+
     return
