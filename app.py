77f1ad8d3880824730a1678c6e367b386631d3c6
Kevin Renner <krenner529@gmail.com>
2017-02-25 03:30:41 -0600

    fixing stuff

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import bottle
 import os
 from os import environ
-
-# routes contains the HTTP handlers for our server and must be imported.
 import routes
 
 def wsgi_app():
@@ -14,6 +12,4 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('PORT', '5000'))
     except ValueError:
         PORT = 5555
-
-    # Starts a local test server.
     bottle.run(server='gunicorn', host=HOST, port=PORT)
