16949f574777749d1c6bfcc1e8f125b311072835
Kevin Renner <krenner529@gmail.com>
2017-02-25 16:51:35 -0600

    debug

--- routes.py
+++ routes.py
@@ -30,11 +30,12 @@ def defaut():
         attachment_type = data["attachments"][0]["type"]
         uid = match_image(url)
         nickname = get_name_from_uid(uid, groupid)
-        r = requests.post("https://api.groupme.com/v3/bots/post", data={'bot_id':os.environ['BOT_KEY'], 'text': nickname, 'attachment': [{"type": "mentions", "user_ids":[uid], "loci": [[0, len(nickname)]]}]})
+        r = requests.post("https://api.groupme.com/v3/bots/post", data={'bot_id':os.environ['BOT_KEY'], 'text': nickname, 'attachments': [{"type": "mentions", "user_ids":[uid], "loci": [[0, len(nickname)]]}]})
 
     if message.lower() == "this is me":
         save_image(user, url)
 
+    print data
     print user
     print message
 
